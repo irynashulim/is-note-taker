f63e1c8f27971df182d302af68ff9220b4c0ad25
Iryna <iryna.shulim@gmail.com>
2021-07-17 06:38:51 -0500

    create new note function

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const { notes } = require('./data/notes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+
+function createNewNote(body, notesArray) {
+    const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+      path.join(__dirname, './data/notes.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+  );
+    return note;
+  }
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    
+    res.json(results);
+  });
+
+
+ app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+  res.json(req.note);
+});
+
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
