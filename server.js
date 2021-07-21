dc844fce247f96c60ea1edc24466149d06af55fb
Iryna <iryna.shulim@gmail.com>
2021-07-20 19:09:44 -0500

    restructuring/lib

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes')
@@ -12,25 +11,6 @@ app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-
-
-function createNewNote(body, notesArray) {
-  const note = body;
-notesArray.push(note);
-fs.writeFileSync(
-    path.join(__dirname, './data/notes.json'),
-    JSON.stringify({ notes: notesArray }, null, 2)
-);
-  return note;
-}
-
-
-// function findById(id, notesArray) {
-//     const result = notesArray.filter(note => note.id === id)[0];
-//     return result;
-// }
-
-
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
